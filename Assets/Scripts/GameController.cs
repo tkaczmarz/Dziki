8cdbbd4b510acb0e29f0f24a2ffa0de90def5a2e
tkaczmarz <kaczmarzt@gmail.com>
2017-10-15 17:39:13 +0200

    Created first simple map. Added field selection.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject selectionMarkerPrefab;
+    public Grid grid;
+
+    private Transform selectionMarker;
+
+    private void Start()
+    {
+        if (!grid)
+            grid = FindObjectOfType<Grid>();
+
+        if (!selectionMarkerPrefab)
+        {
+            Debug.LogError("Selection marker missing!");
+            Debug.Break();
+        }
+        else
+        {
+            Vector3 cellPos = grid.CellToWorld(Vector3Int.zero);
+            selectionMarker = Instantiate(selectionMarkerPrefab, cellPos, Quaternion.Euler(Vector3.zero)).transform;
+        }
+    }
+
+    private void Update()
+    {
+        MoveSelection();
+    }
+
+    private void MoveSelection()
+    {
+        if (!selectionMarker)
+            return;
+        
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int intMousePos = new Vector3Int(Mathf.FloorToInt(mousePos.x), Mathf.FloorToInt(mousePos.y), Mathf.FloorToInt(mousePos.z));
+        Vector3 cellPos = grid.CellToWorld(intMousePos);
+        selectionMarker.position = cellPos;
+    }
+}
