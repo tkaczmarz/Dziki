09852f821ff193fbc876ab0557e72ebce752eeb0
tkaczmarz <kaczmarzt@gmail.com>
2017-10-29 23:52:37 +0100

    Basic unit navigation

--- Assets/Scripts/MapController.cs
+++ Assets/Scripts/MapController.cs
@@ -4,5 +4,44 @@ using UnityEngine;
 
 public class MapController : MonoBehaviour
 {
-    
+    public static MapController Instance { get { return instance; } }
+    private static MapController instance = null;
+
+    private List<List<Field>> fields = new List<List<Field>>();
+
+    private void Awake() 
+    {
+        if (!instance)
+        {
+            instance = this;
+        }
+        else
+            DestroyImmediate(gameObject);
+
+        // get map data
+        foreach (Transform row in transform)
+        {
+            fields.Add(new List<Field>());
+            foreach (Field field in row.GetComponentsInChildren<Field>())
+            {
+                fields[fields.Count - 1].Add(field);
+            }
+        }
+    }
+
+    public Field GetFieldAt(int x, int y)
+    {
+        if (y >= fields.Count || y < 0)
+        {
+            Debug.LogWarning("Wrong field coordinates!");
+            return null;
+        }
+        else if (x >= fields[y].Count || x < 0)
+        {
+            Debug.LogWarning("Wrong field coordinates!");
+            return null;
+        }
+
+        return fields[y][x];
+    }
 }
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Unit : MonoBehaviour 
+{
+	public float moveSpeed = 4;
+	public TerrainType[] walkableTerrains;
+
+	private int walkableMask = 0;
+
+	private void Awake() 
+	{		
+		foreach (TerrainType terrain in walkableTerrains)
+		{
+			walkableMask |= (1 << NavMesh.GetAreaFromName(terrain.ToString()));
+		}
+
+		// place agent on NavMesh
+		NavMeshHit navMeshHit = new NavMeshHit();
+		if (NavMesh.SamplePosition(transform.position, out navMeshHit, 5, walkableMask))
+		{
+			transform.position = navMeshHit.position;
+			transform.eulerAngles = new Vector3(90, 0, 0);
+		}
+		else
+			Debug.LogWarning("Can't place agent on a NavMesh!");
+	}
+
+	private void Update() 
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			int x = Mathf.RoundToInt(mousePos.x);
+			int y = Mathf.RoundToInt(mousePos.z);
+			GoTo(x, y);
+		}	
+	}
+
+	// calculates path to target location
+	public void GoTo(int x, int y)
+	{
+		Vector3 targetPos = new Vector3(x, 0, y);
+		NavMeshPath path = new NavMeshPath();
+		NavMesh.CalculatePath(transform.position, targetPos, walkableMask, path);
+		StopAllCoroutines();
+		StartCoroutine(Move(path));
+	}
+
+	// moves unit through given path
+	private IEnumerator Move(NavMeshPath path)
+	{
+		foreach (Vector3 corner in path.corners)
+		{
+			while (Vector3.Distance(transform.position, corner) > 0.05f)
+			{
+				transform.position = Vector3.MoveTowards(transform.position, corner, Time.deltaTime * moveSpeed);
+				yield return null;
+			}
+
+			transform.position = corner;
+		}
+	}
+}
